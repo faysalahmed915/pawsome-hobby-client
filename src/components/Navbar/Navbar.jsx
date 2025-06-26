9f82f36eccdac10cfa36c8974b619ed01f3ba331
faysalahmed915 <faysalahmed915@gmail.com>
2025-06-26 23:09:59 +0600

    logo added as a component

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,6 +5,7 @@ import toast from 'react-hot-toast';
 import { AuthContext } from '../../Provider/AuthProvider';
 import PhotoUser from '../Profile/PhotoUser';
 import ThemeToggle from '../Theme/ThemeToggle';
+import Logo from '../Shared/logo';
 
 const Navbar = () => {
     const { user, logOut, loading } = use(AuthContext);
@@ -44,65 +45,71 @@ const Navbar = () => {
         // </>
     );
     return (
-        <div className="navbar bg-primary shadow-md px-4 lg:px-8 py-2 sticky top-0 z-50">
-            <div className="navbar-start">
-                <div className="dropdown">
-                    <div tabIndex={0} role="button" className="btn btn-ghost text-gray-600 lg:hidden hover:bg-gray-100">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" />
-                        </svg>
+        <div className="bg-primary shadow-md  lg:px-8 py-2 sticky top-0 z-50">
+            <div className="navbar max-w-7xl mx-auto">
+                <div className="navbar-start">
+                    <div className="dropdown">
+                        <div tabIndex={0} role="button" className="btn btn-ghost text-gray-600 lg:hidden hover:bg-gray-100">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" />
+                            </svg>
+                        </div>
+                        <ul
+                            tabIndex={0}
+                            className="menu menu-sm dropdown-content mt-3 p-2 shadow-lg bg-white rounded-lg w-52 text-gray-700 space-y-1 z-50"
+                        >
+                            {links}
+                        </ul>
                     </div>
-                    <ul
-                        tabIndex={0}
-                        className="menu menu-sm dropdown-content mt-3 p-2 shadow-lg bg-white rounded-lg w-52 text-gray-700 space-y-1 z-50"
-                    >
+
+                    {/* <Link to="/" className="text-xl font-bold hidden lg:flex items-center gap-2">
+                        <FaPaw className="text-[#176AE5] text-2xl" /> Pawsome Hobby
+                    </Link> */}
+                    <div className='w-24 hidden lg:flex items-center'>
+                        <Logo></Logo>
+                    </div>
+                </div>
+
+                <div className="navbar-center hidden lg:flex">
+                    <ul className="menu menu-horizontal px-1 gap-5 text-gray-600 font-medium">
                         {links}
                     </ul>
                 </div>
-                <Link to="/" className="text-xl font-bold hidden lg:flex items-center gap-2">
-                    <FaPaw className="text-[#176AE5] text-2xl" /> Pawsome Hobby
-                </Link>
-            </div>
 
-            <div className="navbar-center hidden lg:flex">
-                <ul className="menu menu-horizontal px-1 gap-5 text-gray-600 font-medium">
-                    {links}
-                </ul>
-            </div>
+                <div className="navbar-end gap-3">
+                    {user ? (
+                        <PhotoUser />
+                    ) : (
+                        <span className="text-sm text-gray-600">{user?.email}</span>
+                    )}
 
-            <div className="navbar-end gap-3">
-                {user ? (
-                    <PhotoUser />
-                ) : (
-                    <span className="text-sm text-gray-600">{user?.email}</span>
-                )}
-
-                {user ? (
-                    <button
-                        onClick={handleLogOut}
-                        className="btn rounded-full bg-[#176AE5] text-white hover:bg-[#145bcc] px-5"
-                    >
-                        <Link to="/login">Logout</Link>
-                    </button>
-                ) : (
-                    <div className="flex gap-2">
-                        <Link
-                            to="/login"
+                    {user ? (
+                        <button
+                            onClick={handleLogOut}
                             className="btn rounded-full bg-primary text-white hover:bg-[#145bcc] px-5"
                         >
-                            Login
-                        </Link>
-                        <Link
-                            to="/signUp"
-                            className="btn rounded-full bg-primary text-white hover:bg-[#145bcc] px-5"
-                        >
-                            Sign Up
-                        </Link>
-                    </div>
-                )}
-                
+                            <Link to="/login">Logout</Link>
+                        </button>
+                    ) : (
+                        <div className="flex gap-2">
+                            <Link
+                                to="/login"
+                                className="btn rounded-full bg-primary text-white hover:bg-[#145bcc] px-5"
+                            >
+                                Login
+                            </Link>
+                            <Link
+                                to="/signUp"
+                                className="btn rounded-full bg-primary text-white hover:bg-[#145bcc] px-5"
+                            >
+                                Sign Up
+                            </Link>
+                        </div>
+                    )}
+
                     <ThemeToggle />
-                
+
+                </div>
             </div>
         </div>
 
---
+++ src/components/Shared/Logo.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router';
+
+const Logo = () => {
+    return (
+        <div>
+            <Link to="/">
+                <img src="https://i.ibb.co/SGHPtWx/Paw-Some-Logo.png" alt="" />
+            </Link>
+        </div>
+    );
+};
+
+export default Logo;
