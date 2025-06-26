f056b1280fcad018449efa190e6d90a28133578a
faysalahmed915 <faysalahmed915@gmail.com>
2025-06-26 16:49:08 +0600

    error page update

--- src/components/ErrorPage/ErrorPage.jsx
+++ src/components/ErrorPage/ErrorPage.jsx
@@ -8,7 +8,7 @@ const ErrorPage = () => {
             <div className="bg-white p-10 rounded-2xl shadow-xl">
                 <h1 className="text-6xl font-bold text-red-500 mb-4">{error?.status || 404}</h1>
                 <p className="text-xl text-gray-700 mb-2">Oops! Page not found.</p>
-                <h1 className="text-6xl font-bold text-red-500 mb-4">Sometimes it works on multiple refresh due to poor vercel server connection</h1>
+                {/* <h1 className="text-6xl font-bold text-red-500 mb-4">Sometimes it works on multiple refresh due to poor vercel server connection</h1> */}
                 <p className="text-gray-500">The page you're looking for doesn't exist.
                     {/* <br />
                     So don't waste time and <span className='text-blue-600 shadow'>learn some code</span>. Go home now */}
